b489dc1b47666d5b3c2f6ce8823e81151dc6e03b
JensYvanDeCraecker <jens.de.craecker@outlook.be>
2018-01-23 12:38:50 +0100

    Casual work.

---
+++ SimplePipeline/SimplePipeline.Examples/BuilderPipelineExample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SimplePipeline.Builder;
+
+namespace SimplePipeline.Examples
+{
+    public static class BuilderPipelineExample
+    {
+        public static void Demo()
+        {
+            BuilderPipeline<String, String> pipeline = BuilderPipeline.Chain(Filter.Create<String, String>(input => input.ToUpper())).Chain(Filter.Create<String, String>(input => input.Trim()));
+        }
+    }
+}
