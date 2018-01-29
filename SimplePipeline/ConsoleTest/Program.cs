f81d411c01c0bea0909a3ea8a4ce3ff0dbfdc9c8
JensYvanDeCraecker <jens.de.craecker@outlook.be>
2018-01-29 09:38:44 +0100

    Updated demo code.

--- SimplePipeline/ConsoleTest/Program.cs
+++ SimplePipeline/ConsoleTest/Program.cs
@@ -9,8 +9,8 @@ namespace ConsoleTest
     {
         private static void Main()
         {
-            IPipeline<String, Byte[]> demoPipelineLambda = PipelineBuilder.Create<String, Byte[]>(builder => builder.Chain(new TrimFilter()).Chain(((Func<String, Byte[]>) (input => Encoding.Unicode.GetBytes(input))).ToFilter()).Chain(new HashingFilter()));
-            IPipeline<String, Byte[]> demoPipeline = new PipelineBuilder<String>().Chain(new TrimFilter()).Chain(((Func<String, Byte[]>) (input => Encoding.Unicode.GetBytes(input))).ToFilter()).Chain(new HashingFilter()).Build();
+            IPipeline<String, Byte[]> demoPipelineLambda = PipelineBuilder.Create<String, Byte[]>(builder => builder.Chain(new TrimFilter()).Chain(((Func<String, Byte[]>)(input => Encoding.Unicode.GetBytes(input))).ToFilter()).Chain(new HashingFilter()));
+            IPipeline<String, Byte[]> demoPipeline = PipelineBuilder.Chain(new TrimFilter()).Chain(((Func<String, Byte[]>)(input => Encoding.Unicode.GetBytes(input))).ToFilter()).Chain(new HashingFilter()).Build();
             IPipeline<String, String> pipeline = new Pipeline<String>()
             {
                 ((Func<String, String>) (input => input.Trim())).ToFilter(),
