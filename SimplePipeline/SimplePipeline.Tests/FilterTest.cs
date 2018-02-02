2ecf0bf9decbbffb9a76b61faf27fb13a967093a
JensYvanDeCraecker <jens.de.craecker@outlook.be>
2018-02-02 09:23:08 +0100

    Creating tests

--- SimplePipeline/SimplePipeline.Tests/FilterTest.cs
+++ SimplePipeline/SimplePipeline.Tests/FilterTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using NUnit.Framework;
 
 namespace SimplePipeline.Tests
@@ -8,7 +10,7 @@ namespace SimplePipeline.Tests
     [TestFixture]
     public class FilterTest
     {
-        public static IEnumerable ExecuteFilterData
+        public static IEnumerable<TestCaseData> ExecuteFilterData
         {
             get
             {
@@ -16,14 +18,26 @@ namespace SimplePipeline.Tests
                 yield return new TestCaseData(((Func<String, String>)(input => input.Replace(" ", ""))).ToFilter(), "Jens Yvan De Craecker").Returns("JensYvanDeCraecker");
                 yield return new TestCaseData(((Func<String, String>)(input => input.ToLower())).ToFilter(), "Jens Yvan De Craecker").Returns("jens yvan de craecker");
                 yield return new TestCaseData(((Func<String, String>)(input => input.Substring(0, 4))).ToFilter(), "Jens Yvan De Craecker").Returns("Jens");
+                yield return new TestCaseData(((Func<String, String>)(input => input?.Substring(0, 4))).ToFilter(), null).Returns(null);
+                yield return new TestCaseData(((Func<String, String>)(input => Convert.ToBase64String(new SHA256CryptoServiceProvider().ComputeHash(Encoding.Unicode.GetBytes(input))))).ToFilter(), "Jens Yvan De Craecker").Returns("lTeckkBgmzBUOz8vc+hsBCZKSwJWdnPpP+d92vbo1os=");
+                yield return new TestCaseData(((Func<String, String>)(input => input.ToUpper())).ToFilter(), "Jens Yvan De Craecker").Returns("JENS YVAN DE CRAECKER");
+                yield return new TestCaseData(((Func<String, Int32>)(input => input.Length)).ToFilter(), "Jens Yvan De Craecker").Returns(21);
             }
         }
 
         [Test]
         [TestCaseSource(nameof(ExecuteFilterData))]
-        public String ExecuteFilter(IFilter<String, String> filter, String input)
+        public TOutput ExecuteFilter<TInput, TOutput>(IFilter<TInput, TOutput> filter, TInput input)
         {
-            return filter.Execute(input);
+            try
+            {
+                return filter.Execute(input);
+            }
+            catch (Exception)
+            {
+                return default(TOutput);
+            }
+          
         }
     }
 }
---
+++ SimplePipeline/SimplePipeline.Tests/PipelineTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SimplePipeline.Tests
+{
+    [TestFixture]
+    public class PipelineTest
+    {
+        [Test]
+        [TestCaseSource(nameof(ExecutePipelineData))]
+        public TOutput ExecutePipeline<TInput, TOutput>(IPipeline<TInput, TOutput> pipeline, TInput input)
+        {
+            return pipeline.Execute(input) ? pipeline.Output : default(TOutput);
+        }
+
+        public static IEnumerable<TestCaseData> ExecutePipelineData
+        {
+            get
+            {
+                yield return new TestCaseData(new Pipeline<String, String>()
+                {
+                    ((Func<String, String>)(input => input.ToLower())).ToFilter(),
+                    ((Func<String, String>)(input => new String(input.Reverse().ToArray()))).ToFilter(),
+                    ((Func<String, String>)(input => input.Substring(0, 4))).ToFilter()
+                }, "Jens Yvan De Craecker").Returns("rekc");
+            }
+        }
+    }
+}
