403ee81bd039c1fa3b57baf0e6af3b3bc61cecaa
JensYvanDeCraecker <jens.de.craecker@outlook.be>
2018-02-02 06:40:34 +0100

    Recreated tests project as a .NET Framework project

---
+++ SimplePipeline/SimplePipeline.Tests/FilterTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SimplePipeline.Tests
+{
+    [TestFixture]
+    public class FilterTest
+    {
+        [Test]
+        [TestCaseSource(typeof(FilterTestData), nameof(FilterTestData.TestCases))]
+        public String CreateFilterAndExecute(Func<String, String> func, string input)
+        {
+            IFilter<String, String> filter = func.ToFilter();
+            return filter.Execute(input);
+        }
+    }
+
+}
---
+++ SimplePipeline/SimplePipeline.Tests/FilterTestData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SimplePipeline.Tests
+{
+    public class FilterTestData
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData((Func<String, String>)(input => new String(input.Reverse().ToArray())), "Jens Yvan De Craecker").Returns("rekcearC eD navY sneJ");
+                yield return new TestCaseData((Func<String, String>)(input => input.Replace(" ", "")), "Jens Yvan De Craecker").Returns("JensYvanDeCraecker");
+                yield return new TestCaseData((Func<String, String>)(input => input.ToLower()), "Jens Yvan De Craecker").Returns("jens yvan de craecker");
+                yield return new TestCaseData((Func<String, String>)(input => input.Substring(0, 4)), "Jens Yvan De Craecker").Returns("Jens");
+            }
+        }
+    }
+}
