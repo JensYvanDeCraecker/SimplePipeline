da458812f87f79c9544ec2d026ea3556192c1e54
JensYvanDeCraecker <jens.de.craecker@outlook.be>
2018-02-14 11:13:59 +0100

    Added FilterTest class
    Added FilterTest class to test if an exception is thrown when null is passed as a parameter.

---
+++ SimplePipeline/SimplePipeline.Tests/FilterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SimplePipeline.Tests
+{
+    [TestFixture]
+    public class FilterTest
+    {
+        [Test]
+        public void ToFilterFuncNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => ((Func<String,String>)null).ToFilter());
+        }
+        [Test]
+        public void ToFilterPipelineNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => ((IPipeline<String, String>)null).ToFilter());
+        }
+    }
+}
