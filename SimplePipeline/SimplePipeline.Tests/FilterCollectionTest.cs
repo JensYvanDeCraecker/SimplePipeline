0fa86a60e2ee89b0fa5c89d65308ce63cd72a16a
JensYvanDeCraecker <jens.de.craecker@outlook.be>
2018-02-23 13:42:00 +0100

    Removed repeated code in FilterCollectionTest

--- SimplePipeline/SimplePipeline.Tests/FilterCollectionTest.cs
+++ SimplePipeline/SimplePipeline.Tests/FilterCollectionTest.cs
@@ -15,18 +15,31 @@ namespace SimplePipeline.Tests
             collection.Add(filter);
         }
 
-        [Test]
-        [TestCaseSource(typeof(TestData), nameof(TestData.PossibleFilterCollectionData))]
-        public void PossibleFilterCollection(IEnumerable<Object> items)
+        public void FillSequence(IEnumerable<Object> items)
         {
-            FilterCollection collection = new FilterCollection();
-            Assert.DoesNotThrow(() =>
+            try
             {
+                FilterCollection collection = new FilterCollection();
                 foreach (Object item in items)
                     if (item is Tuple<Object, Type, Type> tuple)
                         addFilterDefenition.MakeGenericMethod(tuple.Item2, tuple.Item3).Invoke(this, new[] { collection, tuple.Item1 });
                     else
                         collection.Add((FilterData)item);
+            }
+            catch (TargetInvocationException e)
+            {
+                // ReSharper disable once PossibleNullReferenceException
+                throw e.InnerException;
+            }
+        }
+
+        [Test]
+        [TestCaseSource(typeof(TestData), nameof(TestData.PossibleFilterCollectionData))]
+        public void PossibleFilterCollection(IEnumerable<Object> items)
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                FillSequence(items);
             });
         }
 
@@ -34,22 +47,9 @@ namespace SimplePipeline.Tests
         [TestCaseSource(typeof(TestData), nameof(TestData.UnpossibleFilterCollectionData))]
         public void UnpossibleFilterCollection(IEnumerable<Object> items)
         {
-            FilterCollection collection = new FilterCollection();
             Assert.Throws<InvalidFilterException>(() =>
             {
-                try
-                {
-                    foreach (Object item in items)
-                        if (item is Tuple<Object, Type, Type> tuple)
-                            addFilterDefenition.MakeGenericMethod(tuple.Item2, tuple.Item3).Invoke(this, new[] { collection, tuple.Item1 });
-                        else
-                            collection.Add((FilterData)item);
-                }
-                catch (TargetInvocationException e)
-                {
-                    // ReSharper disable once PossibleNullReferenceException
-                    throw e.InnerException;
-                }
+                FillSequence(items);
             });
         }
     }
