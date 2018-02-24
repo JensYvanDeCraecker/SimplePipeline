016feee149bd5acb9501c9991a3431bcdd95132d
JensYvanDeCraecker <jens.de.craecker@outlook.be>
2018-02-24 20:32:46 +0100

    Removed comments, added testcasesource to equals false in fitlerdata tests

--- SimplePipeline/SimplePipeline.Tests/FilterDataTest.cs
+++ SimplePipeline/SimplePipeline.Tests/FilterDataTest.cs
@@ -1,36 +1,10 @@
-﻿using System;
-using System.Reflection;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace SimplePipeline.Tests
 {
     [TestFixture]
     public class FilterDataTest
     {
-        //private readonly MethodInfo processFilterDataEqualityDefinition = typeof(FilterDataTest).GetMethod("ProcessFilterDataEquality", BindingFlags.NonPublic | BindingFlags.Instance);
-
-        //// ReSharper disable once UnusedMember.Local
-        //private void ProcessFilterDataEquality<TFilterInput, TFilterOutput>(IFilter<TFilterInput, TFilterOutput> filter)
-        //{
-        //    Assert.AreEqual(FilterData.Create(filter), FilterData.Create(filter));
-        //}
-
-        //[Test]
-        //[TestCaseSource(typeof(TestData), nameof(TestData.CompareFilterTypeData))]
-        //public void CompareFilterType(FilterData data)
-        //{
-        //    Type expectedFilterType = typeof(IFilter<,>).MakeGenericType(data.InputType, data.OutputType);
-        //    Assert.AreEqual(expectedFilterType, data.FilterType);
-        //    Assert.IsTrue(expectedFilterType.IsInstanceOfType(data.Filter));
-        //}
-
-        //[Test]
-        //[TestCaseSource(typeof(TestData), nameof(TestData.FilterDataEqualityData))]
-        //public void FilterDataEquality(Object filter, Type filterInputType, Type filterOutputType)
-        //{
-        //    processFilterDataEqualityDefinition.MakeGenericMethod(filterInputType, filterOutputType).Invoke(this, new[] { filter });
-        //}
-
         [Test]
         [TestCaseSource(typeof(TestData), nameof(TestData.FilterDataEqualsTrueData))]
         public void FilterDataEqualsTrue(FilterData firstFilterData, FilterData secondFilterData)
@@ -38,6 +12,8 @@ namespace SimplePipeline.Tests
             Assert.IsTrue(Equals(firstFilterData, secondFilterData));
         }
 
+        [Test]
+        [TestCaseSource(typeof(TestData), nameof(TestData.FilterDataEqualsFalseData))]
         public void FilterDataEqualsFalse(FilterData firstFilterData, FilterData secondFilterData)
         {
             Assert.IsFalse(Equals(firstFilterData, secondFilterData));
