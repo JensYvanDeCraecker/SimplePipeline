5cbe172332949ec8b82780bd6f7cb7ed6b5f346a
JensYvanDeCraecker <jens.de.craecker@outlook.be>
2018-03-03 02:51:59 +0100

    Created FilterDataTest

---
+++ SimplePipeline/SimplePipeline.Tests.X/FilterDataTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using SimplePipeline.Tests.Filters;
+using Xunit;
+
+namespace SimplePipeline.Tests.X
+{
+    public class FilterDataTest
+    {
+        private readonly MethodInfo processCreateFilterDataDefinition = typeof(FilterDataTest).GetMethod("ProcessCreateFilterData", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        // Syntax: IFilter<in TInput, out TOutput>, TInput type, TOutput type
+        public static IEnumerable<Object[]> Filters
+        {
+            get
+            {
+                yield return new Object[] { new CharEnumerableToStringFilter(), typeof(IEnumerable<Char>), typeof(String) };
+                yield return new Object[] { new CharEnumerableToStringFilter(), typeof(IEnumerable<Char>), typeof(IEnumerable<Char>) };
+                yield return new Object[] { new CharEnumerableToStringFilter(), typeof(String), typeof(String) };
+                yield return new Object[] { new CharEnumerableToStringFilter(), typeof(String), typeof(IEnumerable<Char>) };
+            }
+        }
+
+        // Syntax: FilterData, FilterData, Boolean
+        public static IEnumerable<Object[]> FilterDataPairs
+        {
+            get
+            {
+                IFilter<IEnumerable<Char>, String> charEnumerableToString = new CharEnumerableToStringFilter();
+                yield return new Object[] { FilterData.Create(charEnumerableToString), FilterData.Create(charEnumerableToString), true };
+                yield return new Object[] { FilterData.Create(charEnumerableToString), FilterData.Create<IEnumerable<Char>, IEnumerable<Char>>(charEnumerableToString), false };
+                yield return new Object[] { FilterData.Create(charEnumerableToString), FilterData.Create<String, String>(charEnumerableToString), false };
+                yield return new Object[] { FilterData.Create(charEnumerableToString), FilterData.Create<String, IEnumerable<Char>>(charEnumerableToString), false };
+            }
+        }
+
+        // Syntax: FilterData, Object, Object
+        public static IEnumerable<Object[]> FilterDatas
+        {
+            get
+            {
+                yield return new Object[] { FilterData.Create(new CharEnumerableToStringFilter()), new[] { 'S', 'i', 'm', 'p', 'l', 'e', 'P', 'i', 'p', 'e', 'l', 'i', 'n', 'e' }, "SimplePipeline" };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(Filters))]
+        public void CreateFilterData(Object filter, Type filterInputType, Type filterOutputType)
+        {
+            processCreateFilterDataDefinition.MakeGenericMethod(filterInputType, filterOutputType).Invoke(this, new[] { filter });
+        }
+
+        [Theory]
+        [MemberData(nameof(FilterDataPairs))]
+        public void FilterDataEquality(FilterData firstData, FilterData secondData, Boolean expectedResult)
+        {
+            Assert.Equal(expectedResult, Equals(firstData, secondData));
+            Assert.Equal(expectedResult, Equals(firstData.GetHashCode(), secondData.GetHashCode()));
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        private void ProcessCreateFilterData<TFilterInput, TFilterOutput>(IFilter<TFilterInput, TFilterOutput> filter)
+        {
+            FilterData data = FilterData.Create(filter);
+            Assert.Equal(typeof(IFilter<TFilterInput, TFilterOutput>), data.FilterType);
+            Assert.Equal(typeof(TFilterInput), data.InputType);
+            Assert.Equal(typeof(TFilterOutput), data.OutputType);
+        }
+
+        [Theory]
+        [MemberData(nameof(FilterDatas))]
+        public void FilterDataExecuteFilter(FilterData data, Object filterInput, Object expectedFilterOutput)
+        {
+            Assert.Equal(expectedFilterOutput, data.ExecuteFilter(filterInput));
+        }
+
+        [Fact]
+        public void CreateFilterDataNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => FilterData.Create<Object, Object>(null));
+        }
+
+        [Fact]
+        public void FilterDataExecuteFilterException()
+        {
+            Assert.Throws<Exception>(() => FilterData.Create(((Func<Object, Object>)(input => throw new Exception())).ToFilter()).ExecuteFilter(null));
+        }
+    }
+}
