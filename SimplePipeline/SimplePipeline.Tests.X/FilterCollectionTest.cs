9f5643b2612cdcfd3f8b0e2d634dcd3215825b99
JensYvanDeCraecker <jens.de.craecker@outlook.be>
2018-03-08 02:27:36 +0100

    Added sequence create test

--- SimplePipeline/SimplePipeline.Tests.X/FilterCollectionTest.cs
+++ SimplePipeline/SimplePipeline.Tests.X/FilterCollectionTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using SimplePipeline.Tests.Filters;
 using Xunit;
 
 namespace SimplePipeline.Tests.X
@@ -12,6 +14,39 @@ namespace SimplePipeline.Tests.X
             Assert.False(sequence.CanCreatePipeline(null, typeof(Object)));
             Assert.False(sequence.CanCreatePipeline(typeof(Object), null));
             Assert.False(sequence.CanCreatePipeline(null, null));
+        }
+
+        public static IEnumerable<Object[]> FilterDataCollections
+        {
+            get
+            {
+                yield return new Object[]{ new List<FilterData>()
+                {
+                    FilterData.Create(new CharEnumerableToStringFilter()),
+                    FilterData.Create(new EnumerableCountFilter<Char>())
+                }, true};
+                yield return new Object[]{ new List<FilterData>()
+                {
+                    FilterData.Create(new EnumerableToArrayFilter<Char>()),
+                    FilterData.Create(((Func<String, Int32>)(input => input.Length)).ToFilter())
+                }, false};
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(FilterDataCollections))]
+        public void CreateSequence(IEnumerable<FilterData> datas, Boolean shouldSucceed)
+        {
+            void Test()
+            {
+                FilterCollection sequence = new FilterCollection();
+                foreach (FilterData data in datas)
+                    sequence.Add(data);
+            }
+            if (shouldSucceed)
+                Test();
+            else
+                Assert.Throws<InvalidFilterException>((Action)Test);
         }
     }
 }
