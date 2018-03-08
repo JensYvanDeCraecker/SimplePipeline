6e23f45c40d6cc9f862bc584f89d16c694a93562
JensYvanDeCraecker <jens.de.craecker@outlook.be>
2018-03-08 01:27:31 +0100

    Added FilterCollectionTest class
    - Added class
    - Added a test for CanCreatePipeline method to check if the result is false when any of the parameters is null

---
+++ SimplePipeline/SimplePipeline.Tests.X/FilterCollectionTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace SimplePipeline.Tests.X
+{
+    public class FilterCollectionTest
+    {
+        [Fact]
+        public void CanCreatePipelineParametersNull()
+        {
+            FilterCollection sequence = new FilterCollection();
+            Assert.False(sequence.CanCreatePipeline(null, typeof(Object)));
+            Assert.False(sequence.CanCreatePipeline(typeof(Object), null));
+            Assert.False(sequence.CanCreatePipeline(null, null));
+        }
+    }
+}
