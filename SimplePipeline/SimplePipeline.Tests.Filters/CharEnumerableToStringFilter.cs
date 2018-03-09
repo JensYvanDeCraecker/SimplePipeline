6f6da73bf1b88ce4d4964612e664e5a74e45ea6b
JensYvanDeCraecker <jens.de.craecker@outlook.be>
2018-03-10 00:45:32 +0100

    Code cleanup
    - SimplePipeline.Tests.Filters
    - SimplePipeline.Tests.X

--- SimplePipeline/SimplePipeline.Tests.Filters/CharEnumerableToStringFilter.cs
+++ SimplePipeline/SimplePipeline.Tests.Filters/CharEnumerableToStringFilter.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace SimplePipeline.Tests.Filters
 {
-    public class CharEnumerableToStringFilter : IFilter<IEnumerable<Char>, string>
+    public class CharEnumerableToStringFilter : IFilter<IEnumerable<Char>, String>
     {
         private readonly IFilter<IEnumerable<Char>, Char[]> toArrayFilter = new EnumerableToArrayFilter<Char>();
+
         public String Execute(IEnumerable<Char> input)
         {
             return new String(toArrayFilter.Execute(input));
